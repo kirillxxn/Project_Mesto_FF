a588e37e1460b7743b07a61e609456b58be6c690
Ilya Kirillin <kirillinoff@gmail.com>
2024-02-29 00:40:19 +0300

    FinishWork

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,27 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "Сан-Диего",
+      link: "https://images.unsplash.com/photo-1538360259192-2081a1f28ebb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Дублин",
+      link: "https://images.unsplash.com/photo-1525160354320-d8e92641c563?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Стамбул",
+      link: "https://plus.unsplash.com/premium_photo-1661962322472-e0be5fa94a36?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      name: "Бруклин",
+      link: "https://images.unsplash.com/photo-1625182918997-7f03991c6032?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Брисбен",
+      link: "https://images.unsplash.com/photo-1621135152907-a54980fc4846?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      name: "Токио",
+      link: "https://images.unsplash.com/photo-1601042879364-f3947d3f9c16?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     }
+ 
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
+// Все комментарии добавил для себя, не обращайте внимания =)
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template') // Переменная с шаблоном из Template элемента
 // @todo: DOM узлы
+const contentElement = document.querySelector('.content');
+const placeList = contentElement.querySelector('.places__list')
 
 // @todo: Функция создания карточки
-
+function cardAndDelete(card, deleteCard) {
+  const cardElement = cardTemplate.content.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__image').src = card.link; // Ссылка на изображение
+  cardElement.querySelector('.card__image').alt = card.name; // Имя карточки
+  cardElement.querySelector('.card__title').textContent = card.name; // Заголовок карточки
+  cardElement.querySelector('.card__delete-button').addEventListener('click', function () { // Обработчик события дял удаления карточки
+    deleteCard(cardElement);
+  });
+  return cardElement;
+}
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+// @todo: Добавление карточeк на страницу
+function addCard(card) { // Функция добавления карточек
+  placeList.append(cardAndDelete(card, deleteCard));
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((card) => {
+  addCard(card);
+});
